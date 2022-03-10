39e75ba1181282b0cf67387c7b2caec1b48c5e36
Hugo Lima Romão <hugo8romao@gmail.com>
2022-03-10 19:47:33 -0400

    moving files

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import Container from 'components/Container'
+
+import * as S from './styles'
+
+const Footer = () => (
+  <S.Wrapper>
+    <Container>
+      <p>
+        Desenvolvido por <a href="https://github.com/hugolima03">Hugo Lima </a>e
+      </p>
+      <p>
+        Design por <a href="https://dribbble.com/vmarcosp">Marcos Oliveira</a>
+      </p>
+    </Container>
+  </S.Wrapper>
+)
+
+export default Footer
---
+++ src/types/api.ts
@@ -0,0 +1,127 @@
+export type LogoProps = {
+  alternativeText: string
+  url: string
+}
+
+export type HeaderProps = {
+  title: string
+  description: string
+  button: {
+    label: string
+    url: string
+  }
+  image: {
+    alternativeText: string
+    url: string
+  }
+}
+
+export type SectionAboutProjectProps = {
+  title: string
+  description: string
+  image: {
+    alternativeText: string
+    url: string
+  }
+}
+
+export type TechIcon = {
+  title: string
+  icon: {
+    url: string
+  }
+}
+
+export type SectionTechProps = {
+  title: string
+  techIcons: TechIcon[]
+}
+
+export type SectionConceptsProps = {
+  title: string
+  concepts: {
+    title: string
+  }[]
+}
+
+export type SectionModulesProps = {
+  title: string
+  modules: {
+    title: string
+    subtitle: string
+    description: string
+  }[]
+}
+
+export type SectionAgendaProps = {
+  title: string
+  description: string
+}
+
+export type PricingBoxProps = {
+  totalPrice: number
+  numberInstallments: number
+  priceInstallment: number
+  benefits: string
+  button: {
+    label: string
+    url: string
+  }
+}
+
+export type SocialLink = {
+  title: string
+  url: string
+}
+
+export type Author = {
+  photo: {
+    alternativeText: string
+    url: string
+  }
+  name: string
+  role: string
+  socialLinks: SocialLink[]
+  description: string
+}
+
+export type SectionAboutUsProps = {
+  title: string
+  authors: Author[]
+}
+
+export type Review = {
+  name: string
+  text: string
+  photo: {
+    url: string
+    alternativeText: string
+  }
+}
+
+export type SectionReviewsProps = {
+  title: string
+  reviews: Review[]
+}
+
+export type SectionFaqProps = {
+  title: string
+  questions: {
+    question: string
+    answer: string
+  }[]
+}
+
+export type LandingPageProps = {
+  logo: LogoProps
+  header: HeaderProps
+  sectionAboutProject: SectionAboutProjectProps
+  sectionTech: SectionTechProps
+  sectionConcepts: SectionConceptsProps
+  sectionModules: SectionModulesProps
+  sectionAgenda: SectionAgendaProps
+  pricingBox: PricingBoxProps
+  sectionAboutUs: SectionAboutUsProps
+  sectionReviews: SectionReviewsProps
+  sectionFaq: SectionFaqProps
+}
